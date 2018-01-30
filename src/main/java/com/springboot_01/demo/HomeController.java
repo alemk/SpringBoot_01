c99b2ee40c8dba6a9f5b3d32146bc865fe5aa40a
alemk <alemseged.hailu@gmail.com>
2018-01-30 13:47:09 -0500

    Initial commit

---
+++ src/main/java/com/springboot_01/demo/HomeController.java
@@ -0,0 +1,12 @@
+package com.springboot_01.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homepage(){
+        return "index";
+    }
+
+}
